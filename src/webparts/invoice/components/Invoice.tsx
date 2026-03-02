7d0974f0edde7b6d72f37cfad27c03f3ceab8c9a
Venkat-code780 <Venkateshpappula150@gmail.com>
2026-03-02 17:07:45 +0530

    Remove class

--- src/webparts/invoice/components/Invoice.tsx
+++ src/webparts/invoice/components/Invoice.tsx
@@ -15,49 +15,49 @@ export default class InvoiceTracker extends React.Component<IInvoiceProps> {
 
   }
   public componentDidMount() {
-    // this.removeExtraClasses();
+    this.removeExtraClasses();
   }
-// private removeExtraClasses(){
-//   var workbenchElement = document.getElementById("workbenchPageContent");
-//   let wbClass = workbenchElement?.classList.value;
-//   workbenchElement?.classList.remove(wbClass? wbClass : "");
-//   // this.removeAll();
-//   workbenchElement?.addEventListener("click", this.removeAll);
+private removeExtraClasses(){
+  var workbenchElement = document.getElementById("workbenchPageContent");
+  let wbClass = workbenchElement?.classList.value;
+  workbenchElement?.classList.remove(wbClass? wbClass : "");
+  // this.removeAll();
+  workbenchElement?.addEventListener("click", this.removeAll);
  
-// }
+}
  
-// private removeAll = () => {
-//   var workbenchElement = document.getElementById("workbenchPageContent");
-//   workbenchElement?.removeEventListener("click",this.removeAll);
+private removeAll = () => {
+  var workbenchElement = document.getElementById("workbenchPageContent");
+  workbenchElement?.removeEventListener("click",this.removeAll);
  
-//   var canvasComponent1 = document.getElementsByClassName("CanvasZoneContainer");
-//   forEach( canvasComponent1, (element:any) => {
-//     let eleClass = element.classList.value;
-//     let eleClassArr = eleClass.split(" ");
+  var canvasComponent1 = document.getElementsByClassName("CanvasZoneContainer");
+  forEach( canvasComponent1, (element:any) => {
+    let eleClass = element.classList.value;
+    let eleClassArr = eleClass.split(" ");
  
-//     eleClassArr.forEach((elem: string) => {
-//       element.classList.remove(elem.trim());
-//     });
-//   })
-//   var canvasComponent1 = document.getElementsByClassName("CanvasZone");
-//   forEach( canvasComponent1, (element: { classList: { value: any; remove: (arg0: string) => void; }; }) => {
-//     let eleClass = element.classList.value;
-//    let eleClassArr = eleClass.split(" ");
+    eleClassArr.forEach((elem: string) => {
+      element.classList.remove(elem.trim());
+    });
+  })
+  var canvasComponent1 = document.getElementsByClassName("CanvasZone");
+  forEach( canvasComponent1, (element: { classList: { value: any; remove: (arg0: string) => void; }; }) => {
+    let eleClass = element.classList.value;
+   let eleClassArr = eleClass.split(" ");
  
-//     eleClassArr.forEach((elem: string) => {
-//       element.classList.remove(elem.trim());
-//     });
-//   })
-//   var canvasComponent1 = document.getElementsByClassName("CanvasSection");
-//   forEach( canvasComponent1, (element:any) => {
-//     let eleClass = element.classList.value;
-//    let eleClassArr = eleClass.split(" ");
+    eleClassArr.forEach((elem: string) => {
+      element.classList.remove(elem.trim());
+    });
+  })
+  var canvasComponent1 = document.getElementsByClassName("CanvasSection");
+  forEach( canvasComponent1, (element:any) => {
+    let eleClass = element.classList.value;
+   let eleClassArr = eleClass.split(" ");
  
-//     eleClassArr.forEach((elem: string) => {
-//       element.classList.remove(elem.trim());
-//     });
-//   })
-// }
+    eleClassArr.forEach((elem: string) => {
+      element.classList.remove(elem.trim());
+    });
+  })
+}
 
   
   
@@ -86,4 +86,8 @@ export default class InvoiceTracker extends React.Component<IInvoiceProps> {
 
 
 
+
+function forEach(canvasComponent1: HTMLCollectionOf<Element>, arg1: (element: any) => void) {
+  throw new Error('Function not implemented.');
+}
 
